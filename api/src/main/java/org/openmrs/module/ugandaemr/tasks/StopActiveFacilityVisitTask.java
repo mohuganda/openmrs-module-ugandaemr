8f6c32ad5747e3c73919d46467efa5c60cb357e4
Lubwama Samuel <lubwamasamuel3@gmail.com>
2025-03-31 16:59:25 +0300

    U4X-898: Enhance Auto close of visits tasks  to carter for visits with multiple encounter on different days. (#731)

--- api/src/main/java/org/openmrs/module/ugandaemr/tasks/StopActiveFacilityVisitTask.java
+++ api/src/main/java/org/openmrs/module/ugandaemr/tasks/StopActiveFacilityVisitTask.java
@@ -2,6 +2,7 @@ package org.openmrs.module.ugandaemr.tasks;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.Encounter;
 import org.openmrs.Visit;
 import org.openmrs.api.VisitService;
 import org.openmrs.api.context.Context;
@@ -16,32 +17,37 @@ import java.util.List;
 
 public class StopActiveFacilityVisitTask extends AbstractTask {
     protected final Log log = LogFactory.getLog(this.getClass());
+
     @Override
     public void execute() {
         VisitService visitService = Context.getVisitService();
-        PatientQueueingService patientQueueingService=Context.getService(PatientQueueingService.class);
+        PatientQueueingService patientQueueingService = Context.getService(PatientQueueingService.class);
         List<Visit> visitList = visitService.getAllVisits();
-        List<PatientQueue> incompleteQueues = Context.getService(PatientQueueingService.class).getPatientQueueList(null,null,OpenmrsUtil.firstSecondOfDay(new Date()),null,null,null,PatientQueue.Status.PENDING);
-        incompleteQueues = Context.getService(PatientQueueingService.class).getPatientQueueList(null,null,OpenmrsUtil.firstSecondOfDay(new Date()),null,null,null,PatientQueue.Status.PICKED);
+        List<PatientQueue> incompleteQueues = Context.getService(PatientQueueingService.class).getPatientQueueList(null, null, OpenmrsUtil.firstSecondOfDay(new Date()), null, null, null, PatientQueue.Status.PENDING);
+        incompleteQueues = Context.getService(PatientQueueingService.class).getPatientQueueList(null, null, OpenmrsUtil.firstSecondOfDay(new Date()), null, null, null, PatientQueue.Status.PICKED);
 
-        for (PatientQueue patientQueue:incompleteQueues){
+        for (PatientQueue patientQueue : incompleteQueues) {
             try {
                 patientQueueingService.completePatientQueue(patientQueue);
-            }catch (Exception e){
+            } catch (Exception e) {
                 log.error(e);
             }
         }
 
         for (Visit visit : visitList) {
             if (visit.getStopDatetime() == null && visit.getVisitType().getUuid().equals("7b0f5697-27e3-40c4-8bae-f4049abfb4ed") && visit.getStartDatetime().before(OpenmrsUtil.firstSecondOfDay(new Date()))) {
-               try {
-                   visitService.endVisit(visit, OpenmrsUtil.getLastMomentOfDay(visit.getStartDatetime()));
-               }catch (Exception e){
-                   log.error(e);
-               }
+                try {
+                    Date largestEncounterDate = OpenmrsUtil.getLastMomentOfDay(visit.getStartDatetime());
+                    for (Encounter encounter : visit.getEncounters()) {
+                        if (encounter.getEncounterDatetime().after(largestEncounterDate)) {
+                            largestEncounterDate = OpenmrsUtil.getLastMomentOfDay(encounter.getEncounterDatetime());
+                        }
+                    }
+                    visitService.endVisit(visit, largestEncounterDate);
+                } catch (Exception e) {
+                    log.error(e);
+                }
             }
         }
-
-
     }
 }
