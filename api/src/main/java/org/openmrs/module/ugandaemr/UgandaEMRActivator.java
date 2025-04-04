604854db2c34f23946ed2153e20e0bba56d938c1
Mwanje Musa <mwanjemusa@gmail.com>
2025-04-04 09:35:47 +0300

    U4X:913 Fix automatic Upload Failure of Stock Management Metadata

--- api/src/main/java/org/openmrs/module/ugandaemr/UgandaEMRActivator.java
+++ api/src/main/java/org/openmrs/module/ugandaemr/UgandaEMRActivator.java
@@ -239,9 +239,9 @@ public class UgandaEMRActivator extends org.openmrs.module.BaseModuleActivator {
         log.info(" Program related objects Imported");
 
         log.info("Start import of stock item objects");
-        dataImporter.importData(metaDataFilePath + "stock_item.xml");
-        dataImporter.importData(metaDataFilePath + "stock_item_packaging_uom.xml");
-        dataImporter.importData(metaDataFilePath + "stock_item_uom.xml");
+        dataImporter.importData(metaDataFilePath + "stockmanagement/stock_item.xml");
+        dataImporter.importData(metaDataFilePath + "stockmanagement/stock_item_packaging_uom.xml");
+        dataImporter.importData(metaDataFilePath + "stockmanagement/stock_item_uom.xml");
         log.info("stock item  objects Imported");
     }
 }
